5a23e156109f219a028df9a5a307e990eaadd37a
MarkusKiesel93 <markus.kiesel@tuta.io>
2021-09-03 09:10:27 +0200

    write script for testing motor up and down

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class Config(BaseSettings):
     pin_power_supply: int = 21
     pin_power_left: int = 5
     pin_power_right: int = 6
-    pin_polarity_left: List[int] = [13, 19]
-    pin_polarity_right: List[int] = [16, 20]
+    pins_polarity_left: List[int] = [13, 19]
+    pins_polarity_right: List[int] = [16, 20]
 
 
 config = Config()
--- control.py
+++ control.py
@@ -3,39 +3,84 @@ from time import sleep
 
 from config import config
 
+# todo: change exceptions to logging
+
 
 class Control():
     def __init__(self):
         GPIO.setmode(config.gpio_mode)
         GPIO.setwarnings(False)
+        self.waiting_time: int = 2
+        self._setup()
 
-    def test(self):
+    def up(self):
+        for pin in config.pins_polarity_left:
+            self._pin_on(pin)
+        for pin in config.pins_polarity_right:
+            self._pin_on(pin)
+        sleep(self.waiting_time)
+        self._power_on()
+        sleep(self.waiting_time)
+        self._power_off()
+        for pin in config.pins_polarity_left:
+            self._pin_off(pin)
+        for pin in config.pins_polarity_right:
+            self._pin_off(pin)
+        self._teardown()
+
+    def down(self):
+        self._power_on()
+        sleep(self.waiting_time)
+        self._power_off()
+        self._teardown()
+
+    def _power_on(self):
+        self._pin_on(config.pin_power_supply)
+        self._pin_on(config.pin_power_left)
+        self._pin_on(config.pin_power_right)
+
+    def _power_off(self):
+        self._pin_off(config.pin_power_left)
+        self._pin_off(config.pin_power_right)
+        self._pin_off(config.pin_power_supply)
+
+    def _pin_on(self, pin):
         try:
-            GPIO.setup(config.pin_power_left, GPIO.OUT)
-            for pin in config.pin_polarity_left:
-                GPIO.setup(pin, GPIO.OUT)
-            GPIO.output(config.pin_power_left, GPIO.LOW)
-            sleep(3)
-            GPIO.output(config.pin_power_left, GPIO.HIGH)
-            sleep(1)
-            for pin in config.pin_polarity_left:
-                GPIO.output(pin, GPIO.LOW)
-            sleep(1)
-            GPIO.output(config.pin_power_left, GPIO.LOW)
-            sleep(3)
-            GPIO.output(config.pin_power_left, GPIO.HIGH)
-            for pin in config.pin_polarity_left:
-                GPIO.output(pin, GPIO.HIGH)
+            GPIO.output(pin, GPIO.LOW)
         except Exception as ex:
+            print(f'Exception durnig turning on pin: {pin}')
+            print(ex)
+
+    def _pin_off(self, pin):
+        try:
+            GPIO.output(pin, GPIO.HIGH)
+        except Exception as ex:
+            print(f'Exception durnig turning off pin: {pin}')
+            print(ex)
+
+    def _setup(self):
+        try:
+            pins = [
+                config.pin_power_supply,
+                config.pin_power_left,
+                config.pin_power_right,
+                *config.pins_polarity_left,
+                *config.pins_polarity_right]
+            for pin in pins:
+                GPIO.setup(pin, GPIO.OUT)
+        except Exception as ex:
+            print('Exception during setup')
             print(ex)
         finally:
-            self.teardown()
-            
-    def teardown(self):
+            self._teardown()
+
+    def _teardown(self):
         GPIO.cleanup()
 
 
 
 if __name__ == '__main__':
     control = Control()
-    control.test()
+    control.up()
+    sleep(5)
+    control.down()
