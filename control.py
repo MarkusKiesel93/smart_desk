efeafa17350027ede7fa200121c391c597ed1a95
Markus Kiesel <markus.kiesel@tuta.io>
2021-09-24 09:56:56 +0200

    add fixes

--- control.py
+++ control.py
@@ -5,22 +5,29 @@ from config import config
 
 # todo: change exceptions to logging
 
+test_time = 10
 
 class Control():
     def __init__(self):
-        GPIO.setmode(config.gpio_mode)
         GPIO.setwarnings(False)
-        self.waiting_time: int = 2
-        self._setup()
+        self.waiting_time: int = 1
+
+    def test(self):
+        self._pin_on(config.pin_power_supply)
+        sleep(60)
+        self._pin_off(config.pin_power_supply)
+
 
     def up(self):
+        self._pin_on(config.pin_power_supply)
+        sleep(self.waiting_time)
         for pin in config.pins_polarity_left:
             self._pin_on(pin)
         for pin in config.pins_polarity_right:
             self._pin_on(pin)
         sleep(self.waiting_time)
         self._power_on()
-        sleep(self.waiting_time)
+        sleep(test_time)
         self._power_off()
         for pin in config.pins_polarity_left:
             self._pin_off(pin)
@@ -29,23 +36,27 @@ class Control():
         self._teardown()
 
     def down(self):
-        self._power_on()
+        self._pin_on(config.pin_power_supply)
         sleep(self.waiting_time)
+        self._power_on()
+        sleep(test_time)
         self._power_off()
         self._teardown()
 
     def _power_on(self):
-        self._pin_on(config.pin_power_supply)
+        # self._pin_on(config.pin_power_supply)
         self._pin_on(config.pin_power_left)
         self._pin_on(config.pin_power_right)
 
     def _power_off(self):
         self._pin_off(config.pin_power_left)
         self._pin_off(config.pin_power_right)
-        self._pin_off(config.pin_power_supply)
+        # self._pin_off(config.pin_power_supply)
 
     def _pin_on(self, pin):
         try:
+            GPIO.setmode(config.gpio_mode)
+            GPIO.setup(pin, GPIO.OUT)
             GPIO.output(pin, GPIO.LOW)
         except Exception as ex:
             print(f'Exception durnig turning on pin: {pin}')
@@ -53,26 +64,12 @@ class Control():
 
     def _pin_off(self, pin):
         try:
+            GPIO.setmode(config.gpio_mode)
+            GPIO.setup(pin, GPIO.OUT)
             GPIO.output(pin, GPIO.HIGH)
         except Exception as ex:
             print(f'Exception durnig turning off pin: {pin}')
             print(ex)
-
-    def _setup(self):
-        try:
-            pins = [
-                config.pin_power_supply,
-                config.pin_power_left,
-                config.pin_power_right,
-                *config.pins_polarity_left,
-                *config.pins_polarity_right]
-            for pin in pins:
-                GPIO.setup(pin, GPIO.OUT)
-        except Exception as ex:
-            print('Exception during setup')
-            print(ex)
-        finally:
-            self._teardown()
 
     def _teardown(self):
         GPIO.cleanup()
@@ -81,6 +78,7 @@ class Control():
 
 if __name__ == '__main__':
     control = Control()
+    # control.test()
     control.up()
-    sleep(5)
-    control.down()
+    sleep(1)
+    #control.down()
